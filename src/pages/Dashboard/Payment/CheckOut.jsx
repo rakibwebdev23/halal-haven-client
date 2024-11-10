67a71004ad09ad356fd7b88a204cc3094fa64f0a
Rakib <rh.rakibhasan365@gmail.com>
2024-11-11 01:43:45 +0600

    payment intent create

--- src/pages/Dashboard/Payment/CheckOut.jsx
+++ src/pages/Dashboard/Payment/CheckOut.jsx
@@ -1,8 +1,22 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
 
 const CheckOut = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const [error, setError] = useState('');
+    const axiosSecure = useAxiosSecure();
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+    useEffect(() => {
+        axiosSecure.post("/create-payment-intent", {price: totalPrice})
+            .then(res => {
+            console.log("stripe client secret",res.data.clientSecret);
+        })
+    }, [axiosSecure, totalPrice]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -22,10 +36,11 @@ const CheckOut = () => {
         });
 
         if (error) {
-            console.log("payment error issue", error);
+            setError(error.message);
         }
         else {
             console.log("payment method success", paymentMethod);
+            setError('');
         }
     }
 
@@ -48,6 +63,7 @@ const CheckOut = () => {
                         },
                     }}
                 />
+                {error && <p className="text-red-600 mt-4 border-2  border-orange-500 w-1/2 mx-auto rounded-r-full rounded-t-full">{ error}</p>}
                 <button className="bg-orange-600 px-20 py-2 mt-14 text-white font-bold hover:bg-orange-300 rounded-lg hover:text-orange-600" type="submit" disabled={!stripe}>
                     Pay Now
                 </button>
