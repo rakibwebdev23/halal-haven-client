7c1145e9c269b3316fa752eb5387768924361265
Rakib <rh.rakibhasan365@gmail.com>
2025-02-09 17:26:22 +0600

    update popular item

--- src/pages/Home/PopularItem/PopularItem.jsx
+++ src/pages/Home/PopularItem/PopularItem.jsx
@@ -1,3 +1,4 @@
+import Container from "../../../component/Container/Container";
 import MenuCart from "../../../component/MenuCart/MenuCart";
 import MenuData from "../../../component/MenuData/MenuData";
 import SectionTitle from "../../../component/SectionTitle/SectionTitle";
@@ -10,18 +11,20 @@ const PopularItem = () => {
     return (
         <div>
             <BannerPopular></BannerPopular>
-            <SectionTitle
-                subTitle="Popular Menu"
-                title="We provide exclusive food based on BD explore our popular food"
-            ></SectionTitle>
-            <div className="grid gap-10 lg:grid-cols-2 mx-auto lg:mt-20 mt-10 lg:px-8 px-4">
-                {
-                    popularItem.map(item => <MenuCart
-                        key={item._id}
-                        item={item}
-                    ></MenuCart>)
-                }
-            </div>
+            <Container>
+                <SectionTitle
+                    subTitle="Popular Menu"
+                    title="We provide exclusive food based on BD explore our popular food"
+                ></SectionTitle>
+                <div className="grid gap-10 lg:grid-cols-2 mx-auto lg:mt-20 mt-10">
+                    {
+                        popularItem.map(item => <MenuCart
+                            key={item._id}
+                            item={item}
+                        ></MenuCart>)
+                    }
+                </div>
+            </Container>
         </div>
     );
 };
